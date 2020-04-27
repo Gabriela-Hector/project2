c04be40aced131059cc2ca95d3189744de4f7883
Hector Anton <hectoranf@gmail.com>
2020-04-27 22:42:37 +0200

    adjust geolocation and collaboration form

--- routes/collaboration-request.routes.js
+++ routes/collaboration-request.routes.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
-    res.render('collaboration-request/request')
+router.get('/', (req, res) => res.render('collaboration-request/request'))
+
+router.post('/', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/')
 })
 
 module.exports = router
