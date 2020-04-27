0dc6b79655967d4789940696e3b75ee2762b10e6
Hector Anton <hectoranf@gmail.com>
2020-04-27 20:45:21 +0200

    add lat y lng inputs and implements correct values

--- public/javascripts/collaboration-form.js
+++ public/javascripts/collaboration-form.js
@@ -1,33 +1,45 @@
 getLocation = () => {
 
+    const inputLat = document.querySelector('.geolocation #lat')
+    const inputLng = document.querySelector('.geolocation #lng')
     const inputLocation = document.querySelector('.geolocation input')
     const searchBox = new google.maps.places.SearchBox(inputLocation);
+    const geocoder = new google.maps.Geocoder()
 
     if (navigator.geolocation) {
 
         navigator.geolocation.getCurrentPosition(
             position => {
-                console.log("La posición es:", position)
-                const geocoder = new google.maps.Geocoder()
+                inputLat.value = position.coords.latitude
+                inputLng.value = position.coords.longitude
+
                 geocoder.geocode({ 'location': { lat: position.coords.latitude, lng: position.coords.longitude } },
                     (results, status) => {
                         if (status === 'OK') {
                             if (results[0]) {
-                                console.log('La calle es', results[0])
                                 inputLocation.value = results[0].formatted_address
                             }
                         } else {
-                            console.error("No se puedo obtener la localización:")
+                            console.error(`Geocode was not successful for the following reason: ${status}`)
                         }
                     })
             },
-            error => console.error("No se puedo obtener la localización:", error),
+            error => console.error("Couldn't find a location:", error),
             { enableHighAccuracy: true }
         )
     } else {
-        console.error("El navegador no dispone de geolocalizador")
+        console.error("Couldn't use geolocation")
     }
-    
+
     // SearchBox adress is changed
-    // searchBox.addListener('places_changed', () => console.log('el nuevo lugar es', searchBox.getPlaces()))
+    searchBox.addListener('places_changed', () => {
+        geocoder.geocode({ 'address': searchBox.getPlaces()[0].formatted_address }, (results, status) => {
+            if (status === 'OK') {
+                inputLat.value = results[0].geometry.location.lat()
+                inputLng.value = results[0].geometry.location.lng()
+            } else {
+                alert(`Geocode was not successful for the following reason: ${status}`);
+            }
+        })
+    })
 }
