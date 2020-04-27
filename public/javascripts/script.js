352e5d5c8662c41311f6f563c94d7a7d0478bdfa
Gabriela Gallango <ga.gallango@gmail.com>
2020-04-27 17:04:39 +0200

    add map on profile

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
+initMap = () => {
 
-  console.log('IronGenerator JS imported successfully!');
+  let mapOptions = {
+    center: directions.centroMadrid.coords,
+    zoom: 16,
+  }
 
-}, false);
+  const myMap = new google.maps.Map(document.getElementById('map'), mapOptions)
+
+  let markerOptions = {
+    position: directions.centroMadrid.coords,
+    map: myMap,
+    title: directions.centroMadrid.title
+  }
+
+  new google.maps.Marker(markerOptions)
+}
