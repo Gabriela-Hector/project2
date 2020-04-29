56196387fd760bed557c879e2394e55275b9ab46
Gabriela Gallango <ga.gallango@gmail.com>
2020-04-29 19:30:44 +0200

    add info on collabs view

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -6,11 +6,20 @@ const checkLoggedIn = (req, res, next) => req.isAuthenticated() ? next() : res.r
 const User = require("../models/user.model")
 const Collaboration = require("../models/collaboration.model")
 
+
+router.get('/:id/place', (req, res, next) => {
+    Collaboration.findById(req.params.id)
+        .then(data => res.json(data))
+        .catch(err => next(new Error(err)))
+})
+
+
 router.get('/detalles/:id', checkLoggedIn, (req, res, next) => {
     Collaboration.findById(req.params.id)
         .then(collabDet => res.render('profile/collab-details', collabDet))
         .catch(err => next(new Error(err)))
 })
+
 
 router.get('/findCollaborations', (req, res, next) => {
     const knowledgeList = req.query.knowledgeList.split(',')
@@ -32,5 +41,7 @@ router.get('/:username/help', checkLoggedIn, (req, res, next) => {
 router.get('/:username/profile', checkLoggedIn, (req, res, next) => {
     res.render('profile/profile', { user: req.user })
 })
+
+
 
 module.exports = router
